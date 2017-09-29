41aea7963e45dd5f62aa5879817c14335e2552b9
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-09-29 17:17:32 +0900

    Create post_install_2.sh

---
+++ post_install_2.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -x
+
+ls -lang
+
+pwd
+
+mkdir delegate
+chmod 777 delegate
+
+rm -f delegate9.9.13.tar.gz
+wget http://delegate.hpcc.jp/anonftp/DeleGate/delegate9.9.13.tar.gz
+rm -rf delegate9.9.13
+tar xfz delegate9.9.13.tar.gz
+rm -f delegate9.9.13.tar.gz
+pushd delegate9.9.13
+
+export CFLAGS="-march=native -O2"
+export CXXFLAGS="$CFLAGS"
+
+time make -j4 ADMIN="admin@localhost"
+
+cp ./src/delegated ../delegate/
+
+ls -lang ../delegate/
+
+popd
+
+cp ./P8080.conf ./delegate/
+
+chmod 755 ./web-boot.sh
+
+rm -rf delegate9.9.13
+
+zip delegate.zip ./delegate/delegated
+cp delegate.zip www/

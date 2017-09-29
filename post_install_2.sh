66f8c8fe317d55981ba37af88046c95456da7493
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-09-29 17:18:16 +0900

    Update post_install_2.sh

--- post_install_2.sh
+++ post_install_2.sh
@@ -9,29 +9,11 @@ pwd
 mkdir delegate
 chmod 777 delegate
 
-rm -f delegate9.9.13.tar.gz
-wget http://delegate.hpcc.jp/anonftp/DeleGate/delegate9.9.13.tar.gz
-rm -rf delegate9.9.13
-tar xfz delegate9.9.13.tar.gz
-rm -f delegate9.9.13.tar.gz
-pushd delegate9.9.13
-
-export CFLAGS="-march=native -O2"
-export CXXFLAGS="$CFLAGS"
-
-time make -j4 ADMIN="admin@localhost"
-
-cp ./src/delegated ../delegate/
-
-ls -lang ../delegate/
-
-popd
-
 cp ./P8080.conf ./delegate/
 
 chmod 755 ./web-boot.sh
 
-rm -rf delegate9.9.13
+unzip delegate.zip
 
-zip delegate.zip ./delegate/delegated
-cp delegate.zip www/
+cp delegated ./delegate/
+

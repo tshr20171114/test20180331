23fc610963850ed98b1353b92a3c078f03f07311
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-09-29 13:03:00 +0900

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -2,9 +2,13 @@
 
 set -x
 
+ls -lang
+
 chmod 777 delegate
 
+rm -f delegate9.9.13.tar.gz
 wget http://delegate.hpcc.jp/anonftp/DeleGate/delegate9.9.13.tar.gz
+rm -rf delegate9.9.13
 tar xfz delegate9.9.13.tar.gz
 cd delegate9.9.13
 
@@ -16,5 +20,6 @@ time make -j8 ADMIN="admin@localhost"
 ls -lang src/
 
 cp src/delegated /app/delegate/
+cp src/delegated /app/
 
 ls -lang /app/delegate/

6b030c4bf5ee7ee259bcdb78aa6fd88d10d1b624
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-09-29 16:50:44 +0900

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -11,6 +11,7 @@ chmod 777 delegate
 
 rm -f delegate9.9.13.tar.gz
 wget http://delegate.hpcc.jp/anonftp/DeleGate/delegate9.9.13.tar.gz
+rm -f delegate9.9.13.tar.gz
 rm -rf delegate9.9.13
 tar xfz delegate9.9.13.tar.gz
 pushd delegate9.9.13
@@ -29,3 +30,5 @@ popd
 cp ./P8080.conf ./delegate/
 
 chmod 755 ./web-boot.sh
+
+rm -rf delegate9.9.13

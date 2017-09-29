0f0bc09484bc9c379d1b7fd77f33e112cab1705e
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-09-29 10:30:20 +0900

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -5,4 +5,8 @@ echo "STARTTT"
 wget http://delegate.hpcc.jp/anonftp/DeleGate/delegate9.9.13.tar.gz
 tar xfz delegate9.9.13.tar.gz
 cd delegate9.9.13
+
+export CFLAGS="-march=native -O2"
+export CXXFLAGS="$CFLAGS"
+
 time make -j8 ADMIN="admin@localhost"

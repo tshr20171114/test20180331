ac5761a7bcbdb2acb5ef1665462ffe40c5764d48
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-09-29 16:58:02 +0900

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -11,9 +11,9 @@ chmod 777 delegate
 
 rm -f delegate9.9.13.tar.gz
 wget http://delegate.hpcc.jp/anonftp/DeleGate/delegate9.9.13.tar.gz
-rm -f delegate9.9.13.tar.gz
 rm -rf delegate9.9.13
 tar xfz delegate9.9.13.tar.gz
+rm -f delegate9.9.13.tar.gz
 pushd delegate9.9.13
 
 export CFLAGS="-march=native -O2"

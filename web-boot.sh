d6e739d9c55393d7fbd2962feb0a958a34b337ac
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-09-29 17:29:11 +0900

    Update web-boot.sh

--- web-boot.sh
+++ web-boot.sh
@@ -2,8 +2,8 @@
 
 set -x
 
-echo "delegate"
-./delegate/delegated -r +=./delegate/P8080.conf
-
 echo "apache"
 vendor/bin/heroku-php-apache2 -C apache_app.conf www
+
+echo "delegate"
+./delegate/delegated -r -vv +=./delegate/P8080.conf

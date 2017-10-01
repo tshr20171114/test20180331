0923b7172d5cccb9df53e039eb754b7df76f2b7c
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-10-01 21:57:25 +0900

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -2,32 +2,13 @@
 
 set -x
 
-mkdir delegate
-chmod 777 delegate
+mkdir -m 777 icons
+touch icons/index.html
 
 rm -f delegate9.9.13.tar.gz
 wget http://delegate.hpcc.jp/anonftp/DeleGate/delegate9.9.13.tar.gz
 rm -rf delegate9.9.13
 tar xfz delegate9.9.13.tar.gz
 rm -f delegate9.9.13.tar.gz
-pushd delegate9.9.13
-
-export CFLAGS="-march=native -O2"
-export CXXFLAGS="$CFLAGS"
-
-time make -j4 ADMIN="admin@localhost"
-
-cp ./src/delegated ../delegate/
-
-ls -lang ../delegate/
-
-popd
-
-cp ./delegate.conf ./delegate/
-
-chmod 755 ./web-boot.sh
-
+cp ./delegate9.9.13/src/builtin/icons/ysato/*.gif ./icons/
 rm -rf delegate9.9.13
-
-# zip delegate.zip ./delegate/delegated
-# cp delegate.zip www/

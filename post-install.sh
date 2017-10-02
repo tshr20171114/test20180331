44b7b29ca7e5fadc08ebb953ea9eaea2fe8e4f91
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-10-02 12:29:57 +0900

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -2,13 +2,13 @@
 
 set -x
 
-mkdir -m 777 icons
-touch icons/index.html
+mkdir -m 777 www/icons
+touch www/icons/index.html
 
 rm -f delegate9.9.13.tar.gz
 wget http://delegate.hpcc.jp/anonftp/DeleGate/delegate9.9.13.tar.gz
 rm -rf delegate9.9.13
 tar xfz delegate9.9.13.tar.gz
 rm -f delegate9.9.13.tar.gz
-cp ./delegate9.9.13/src/builtin/icons/ysato/*.gif ./icons/
+cp ./delegate9.9.13/src/builtin/icons/ysato/*.gif ./www/icons/
 rm -rf delegate9.9.13

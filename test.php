f355bfafc33076528803bc148d211ca5a1a15dbc
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-03 07:51:12 +0900

    Update test.php

--- test.php
+++ test.php
@@ -3,5 +3,12 @@ $pid = getmypid();
 //$buf = file_get_contents('php://stdin');
 //echo $buf;
 //echo $argc;
-file_get_contents(getenv('TEST_URL') . '?' . $argc);
+//file_get_contents(getenv('TEST_URL') . '?' . $argc);
+
+$stdin = fopen('php://stdin', 'r');
+ob_implicit_flush(true);
+while ($line = fgets($stdin))
+{
+  file_get_contents(getenv('TEST_URL') . '?' . urlencode($line));
+}
 ?>

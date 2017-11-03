87b1a0d552eb31726de393e02dc42e5e38ef6770
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-04 00:28:53 +0900

    Update test.php

--- test.php
+++ test.php
@@ -1,8 +1,5 @@
 <?php
-$stdin = fopen('php://stdin', 'r');
-ob_implicit_flush(true);
-while ($line = fgets($stdin))
-{
-  file_get_contents(getenv('TEST_URL') . '?' . urlencode($line));
-}
+
+
+
 ?>

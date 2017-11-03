f02d720c840f33c1c52bb84c5232443b9aa4d1d6
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-04 00:33:25 +0900

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,18 @@
 <?php
 
+$url = 'https://api.heroku.com/account';
 
+$context = array(
+  "http" => array(
+    "method" => "GET",
+    "header" => array(
+      "Accept: application/vnd.heroku+json; version=3",
+      "Authorization: Bearer " . getenv('API_KEY_01')
+    )
+  )
+);
 
+$res = file_get_contents($url, false, stream_context_create($context));
+
+echo $res;
 ?>

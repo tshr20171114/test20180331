b918daba049bb90503f7e55c07cea4db1a30e9c6
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-04 00:46:43 +0900

    Update test.php

--- test.php
+++ test.php
@@ -14,10 +14,24 @@ $context = array(
 
 $res = file_get_contents($url, false, stream_context_create($context));
 
-echo $res;
-echo "\n";
+echo $res . "\n";
 
 $data = json_decode($res, true);
-echo $data['id'];
+echo $data['id'] . "\n";
 
+$url = 'https://api.heroku.com/accounts/' . $data['id'] . "/actions/get-quota";
+
+$context = array(
+  "http" => array(
+    "method" => "GET",
+    "header" => array(
+      "Accept: application/vnd.heroku+json; version=3.account-quotas",
+      "Authorization: Bearer " . getenv('API_KEY_01')
+    )
+  )
+);
+
+$res = file_get_contents($url, false, stream_context_create($context));
+
+echo $res . "\n";
 ?>

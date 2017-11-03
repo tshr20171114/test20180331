2dab6f6422ba0bd5070b5ae043312bcdddf7fe1f
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-04 02:23:25 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -4,12 +4,14 @@ set -x
 
 date
 
-chmod 755 ./start_web.sh
-
 tar xf phpPgAdmin-5.1.tar.bz2
 
 mv phpPgAdmin-5.1 www/phpphadmin
 
 rm -f phpPgAdmin-5.1.tar.bz2
+
+cp config.inc.php www/phpphadmin/conf/config.inc.php
+
+chmod 755 ./start_web.sh
 
 date

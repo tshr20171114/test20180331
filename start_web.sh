44ae308ba6082178a1ff13d8681db72696445bf4
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-04 07:25:08 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -12,5 +12,17 @@ whereis php
 if [ ! -v LOG_LEVEL ]; then
   export LOG_LEVEL="warn"
 fi
-  
+
+if [ ! -v BASIC_USER ]; then
+  echo "Error : BASIC_USER not defined."
+  exit
+fi
+
+if [ ! -v BASIC_PASSWORD ]; then
+  echo "Error : BASIC_PASSWORD not defined."
+  exit
+fi
+
+htpasswd -c -b .htpasswd ${BASIC_USER} ${BASIC_PASSWORD}
+
 vendor/bin/heroku-php-apache2 -C apache.conf www

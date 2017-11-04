c16c99ced61fbfba2f278828cfbcb1128ec0f8c6
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-05 02:45:46 +0900

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,20 @@
 <?php
 
-$connection_info = parse_url(getenv('DATABASE_URL'));
+// hostname:port:database:username:password
 
-var_dump($connection_info);
+$info = parse_url(getenv('DATABASE_URL'));
 
+/*
+var_dump($info);
+
+echo "\n\n";
+
+echo $info['host'] . "\n\n";
+echo $info['port'] . "\n\n";
+echo $info['path'] . "\n\n";
+echo $info['username'] . "\n\n";
+echo $info['pass'] . "\n\n";
+*/
+
+echo $info['host'] . ':' . $info['port'] . ':' . $info['path'] . ':' . $info['username'] . ':' . $info['pass'];
 ?>

007a9f232b42038115e032fe51d92db3a772770f
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-05 03:30:51 +0900

    Update test.php

--- test.php
+++ test.php
@@ -4,17 +4,5 @@
 
 $info = parse_url(getenv('DATABASE_URL'));
 
-/*
-var_dump($info);
-
-echo "\n\n";
-
-echo $info['host'] . "\n\n";
-echo $info['port'] . "\n\n";
-echo $info['path'] . "\n\n";
-echo $info['username'] . "\n\n";
-echo $info['pass'] . "\n\n";
-*/
-
 echo $info['host'] . ':' . $info['port'] . ':' . substr($info['path'], 1) . ':' . $info['user'] . ':' . $info['pass'];
 ?>

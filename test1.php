4b16f668c55b0f9d0ce573c64b7b5840db9f7248
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-04 10:17:31 +0900

    Update test1.php

--- test1.php
+++ test1.php
@@ -4,7 +4,7 @@ $connection_info = parse_url(getenv('DATABASE_URL'));
 
 $dsn = sprintf('pgsql:host=%s;dbname=%s', $connection_info['host'], substr($connection_info['path'], 1));
 
-$pdo = new PDO($dsn, $url['user'], $url['pass']);
+$pdo = new PDO($dsn, $connection_info['user'], $connection_info['pass']);
 
 $sql = 'SELECT T1.Fqdn FROM M_Appliction T1';
 

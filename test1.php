91a6f599ea2f9b190f3c76027c362716861370f6
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-05 03:32:29 +0900

    Update test1.php

--- test1.php
+++ test1.php
@@ -1,10 +1,9 @@
 <?php
 
-// hostname:port:database:username:password
-// pg_dump -h localhost -U postgres db > dmp
-
 $info = parse_url(getenv('DATABASE_URL'));
 
+echo '#!/bin/bash';
+echo "\n\n";
 echo 'pg_dump --format=plain --schema=public --schema-only --host=' . $info['host'] . ' --username=' . $info['user'] . ' ' . substr($info['path'], 1);
-
+echo "\n\n";
 ?>

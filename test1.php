f0e33ea115df20b01a4b61dc374ee49beb89c9e2
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-04 10:40:20 +0900

    Update test1.php

--- test1.php
+++ test1.php
@@ -8,7 +8,7 @@ echo $dsn . "\n";
 
 $pdo = new PDO($dsn, $connection_info['user'], $connection_info['pass']);
 
-$sql = 'SELECT "FQDN" FROM "M_Application"';
+$sql = 'SELECT "Fqdn" FROM "M_Application"';
 
 $result = $pdo->query($sql);
 

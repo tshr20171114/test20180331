09c7f35c78a4b26391746e9364c56419c84c73c9
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-04 10:15:18 +0900

    Create test1.php

---
+++ test1.php
@@ -0,0 +1,17 @@
+<?php
+
+$connection_info = parse_url(getenv('DATABASE_URL'));
+
+$dsn = sprintf('pgsql:host=%s;dbname=%s', $connection_info['host'], substr($connection_info['path'], 1));
+
+$pdo = new PDO($dsn, $url['user'], $url['pass']);
+
+$sql = 'SELECT T1.Fqdn FROM M_Appliction T1';
+
+foreach ($pdo->query($sql) as $row)
+{
+  echo convert_enc($row['Fqdn']) . "\n";
+}
+
+$pdo = null;
+?>

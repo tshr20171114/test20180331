11da906bd359622ff1846b7c189c0b43d7b765c9
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-04 10:23:10 +0900

    Update test1.php

--- test1.php
+++ test1.php
@@ -4,11 +4,15 @@ $connection_info = parse_url(getenv('DATABASE_URL'));
 
 $dsn = sprintf('pgsql:host=%s;dbname=%s', $connection_info['host'], substr($connection_info['path'], 1));
 
+echo $dsn . "\n";
+
 $pdo = new PDO($dsn, $connection_info['user'], $connection_info['pass']);
 
 $sql = 'SELECT Fqdn FROM M_Appliction';
 
-foreach ($pdo->query($sql) as $row)
+$result = $pdo->query($sql);
+  
+foreach ($result as $row)
 {
   echo convert_enc($row['Fqdn']) . "\n";
 }

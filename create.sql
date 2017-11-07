b4a827fb98e7c67e1d18890d96fe35896d7851e8
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-07 11:26:32 +0900

    Update create.sql

--- create.sql
+++ create.sql
@@ -8,21 +8,10 @@ CREATE TABLE m_application (
     select_type integer NOT NULL
 );
 
-CREATE TABLE t_quota_history (
-    history_seq bigserial PRIMARY KEY,
-    update_time timestamp DEFAULT localtimestamp NOT NULL,
-    quota_previous bigint NOT NULL,
-    quota_current bigint NOT NULL,
-    fqdn character varying(255) NOT NULL
-);
-
 CREATE FUNCTION set_update_time() RETURNS trigger
     LANGUAGE plpgsql
     AS $$begin
   NEW.update_time := localtimestamp;
-
-  INSERT INTO t_quota_history (quota_previous, quota_current, fqdn)
-  VALUES (OLD.dyno_used, NEW.dyno_used, OLD.fqdn);
 
   return NEW;
 end;

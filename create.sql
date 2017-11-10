5ab25a7f83059e3a97222b9a11b0c4a1d647c0ca
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-10 09:55:31 +0900

    Update create.sql

--- create.sql
+++ create.sql
@@ -2,8 +2,11 @@ CREATE TABLE m_application (
     api_key character varying(36) PRIMARY KEY,
     fqdn character varying(255) NOT NULL,
     user_account character varying(255) NOT NULL,
-    dyno_used bigint DEFAULT -1 NOT NULL,
     dyno_quota bigint DEFAULT -1 NOT NULL,
+    dyno_used bigint DEFAULT -1 NOT NULL,
+    dyno_used_previous bigint DEFAULT -1 NOT NULL,
+    update_flag integer DEFAULT 0 NOT NULL,
+    change_time timestamp DEFAULT localtimestamp NOT NULL,
     update_time timestamp DEFAULT localtimestamp NOT NULL,
     select_type integer NOT NULL
 );
@@ -18,12 +21,3 @@ end;
 $$;
 
 CREATE TRIGGER update_trigger BEFORE UPDATE ON m_application FOR EACH ROW EXECUTE PROCEDURE set_update_time();
-
-CREATE VIEW v_rest AS 
-SELECT M1.*
-      ,((M1.dyno_quota - M1.dyno_used) / 86400) d
-      ,(((M1.dyno_quota - M1.dyno_used) / 3600) % (24)::bigint) h
-      ,(((M1.dyno_quota - M1.dyno_used) / 60) % (60)::bigint) m
-      ,date_trunc('month', date 'today' + interval '1 month') - date 'today' rest
-  FROM m_application M1
- ORDER BY M1.dyno_used DESC;

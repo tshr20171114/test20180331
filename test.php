2cf55490fd358ab756a6d9944103c259b6cf6a94
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2017-11-14 21:22:17 +0900

    Delete test.php

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// hostname:port:database:username:password
-
-$info = parse_url(getenv('DATABASE_URL'));
-
-echo $info['host'] . ':' . $info['port'] . ':' . substr($info['path'], 1) . ':' . $info['user'] . ':' . $info['pass'];
-?>

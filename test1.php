3224b9c9dbbc18783a4fbb717ab7c1ec5fbe4e0d
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2017-11-14 21:22:09 +0900

    Delete test1.php

--- test1.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$info = parse_url(getenv('DATABASE_URL'));
-
-echo '#!/bin/bash';
-echo "\n\n";
-echo 'pg_dump --format=plain --schema=public --schema-only --host=' . $info['host'] . ' --username=' . $info['user'] . ' ' . substr($info['path'], 1);
-echo "\n\n";
-?>

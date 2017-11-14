c22e3060e40f31fe3875bfe85559c08455ad63f6
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2017-11-15 01:35:30 +0900

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,23 +1,11 @@
-CREATE TABLE m_application (
-    api_key character varying(36) PRIMARY KEY,
-    fqdn character varying(255) NOT NULL,
-    user_account character varying(255) NOT NULL,
-    dyno_quota bigint DEFAULT -1 NOT NULL,
-    dyno_used bigint DEFAULT -1 NOT NULL,
-    dyno_used_previous bigint DEFAULT -1 NOT NULL,
-    update_flag integer DEFAULT 0 NOT NULL,
-    change_time timestamp DEFAULT localtimestamp NOT NULL,
-    update_time timestamp DEFAULT localtimestamp NOT NULL,
-    select_type integer NOT NULL
+CREATE TABLE t_contents
+(
+ uri character varying(512) NOT NULL PRIMARY KEY,
+ title text NOT NULL,
+ thumbnail character varying(512) NOT NULL,
+ time character varying(6) NOT NULL,
+ create_time timestamp DEFAULT localtimestamp NOT NULL,
+ page character varying(6) NOT NULL
 );
 
-CREATE FUNCTION set_update_time() RETURNS trigger
-    LANGUAGE plpgsql
-    AS $$begin
-  NEW.update_time := localtimestamp;
 
-  return NEW;
-end;
-$$;
-
-CREATE TRIGGER update_trigger BEFORE UPDATE ON m_application FOR EACH ROW EXECUTE PROCEDURE set_update_time();

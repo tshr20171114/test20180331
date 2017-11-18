5b8838bbe8ac06644032b2cef35e8b9052303952
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2017-11-19 07:54:45 +0900

    Update create.sql

--- create.sql
+++ create.sql
@@ -8,6 +8,16 @@ CREATE TABLE t_contents
  page character varying(6) NOT NULL
 );
 
+CREATE TABLE t_contents2
+(
+ uri character varying(512) NOT NULL PRIMARY KEY,
+ title text NOT NULL,
+ thumbnail character varying(512) NOT NULL,
+ time character varying(6) NOT NULL,
+ create_time timestamp DEFAULT localtimestamp NOT NULL,
+ page character varying(6) NOT NULL
+);
+
 CREATE TABLE m_words
 (
  word character varying(256) NOT NULL PRIMARY KEY,

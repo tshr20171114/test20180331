8f9fc2ee0f038afcbfd9647baab09678dba95812
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-01-22 01:15:43 +0900

    Update getrss2.php

--- www/getrss2.php
+++ www/getrss2.php
@@ -156,8 +156,8 @@ do switch (curl_multi_select($mh, 10)) {
     
     do if ($raised = curl_multi_info_read($mh, $remains)) {
       $info = curl_getinfo($raised['handle']);
-      $page = explode('=', parse_url($info[url], PHP_URL_QUERY), 4)[3];
-      $host = parse_url($info[url], PHP_URL_HOST);
+      $page = explode('=', parse_url($info['url'], PHP_URL_QUERY), 4)[3];
+      $host = parse_url($info['url'], PHP_URL_HOST);
       $response = curl_multi_getcontent($raised['handle']);
       //error_log("${pid} ${query_string} " . strlen($response));
       //error_log($response);

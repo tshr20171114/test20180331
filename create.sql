9388ba04ecd62c19a28825aac00c1d1c7173c104
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-02-03 22:16:30 +0900

    Update create.sql

--- create.sql
+++ create.sql
@@ -13,6 +13,7 @@ CREATE TABLE t_contents2
  uri character varying(512) NOT NULL PRIMARY KEY,
  title text NOT NULL,
  thumbnail character varying(512) NOT NULL,
+ thumbnail_hash character varying(256) NOT NULL,
  time character varying(6) NOT NULL,
  create_time timestamp DEFAULT localtimestamp NOT NULL,
  page character varying(6) NOT NULL

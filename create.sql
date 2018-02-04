bd157a81dd80977d15a0a3acb982510edad32fcc
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-02-04 13:33:19 +0900

    Update create.sql

--- create.sql
+++ create.sql
@@ -19,6 +19,13 @@ CREATE TABLE t_contents2
  page character varying(6) NOT NULL
 );
 
+CREATE TABLE t_file_hash
+(
+ thumbnail character varying(512) NOT NULL PRIMARY KEY,
+ thumbnail_hash character varying(256) NOT NULL,
+ create_time timestamp DEFAULT localtimestamp NOT NULL
+);
+
 CREATE TABLE m_words
 (
  word character varying(256) NOT NULL PRIMARY KEY,

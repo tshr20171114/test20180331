7af0ae7dfacee53e657347338325747ee4356ecb
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-03-04 21:33:21 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -30,4 +30,19 @@ fi
 
 htpasswd -c -b .htpasswd ${BASIC_USER} ${BASIC_PASSWORD}
 
+postgres_user=$(echo ${DATABASE_URL} | awk -F':' '{print $2}' | sed -e 's/\///g')
+postgres_password=$(echo ${DATABASE_URL} | grep -o '/.\+@' | grep -o ':.\+' | sed -e 's/://' | sed -e 's/@//')
+postgres_server=$(echo ${DATABASE_URL} | awk -F'@' '{print $2}' | awk -F':' '{print $1}')
+postgres_dbname=$(echo ${DATABASE_URL} | awk -F'/' '{print $NF}')
+
+export PGPASSWORD=${postgres_password}
+
+psql -U ${postgres_user} -d ${postgres_dbname} -h ${postgres_server} > /tmp/sql_result.txt << __HEREDOC__
+DELETE
+  FROM t_file_hash
+ WHERE create_time < localtimestamp - interval '3 days'
+__HEREDOC__
+
+unset PGPASSWORD
+
 vendor/bin/heroku-php-apache2 -C apache.conf www

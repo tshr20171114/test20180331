a5e075a0e9478fb0210678666084c44c09b0190a
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-03-31 20:45:25 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -14,6 +14,11 @@ cp config.inc.php www/phppgadmin/conf/config.inc.php
 
 chmod 755 ./start_web.sh
 
+cd /tmp
 
+wget https://osdn.jp/projects/nkf/downloads/64158/nkf-2.1.4.tar.gz
+tar xf nkf-2.1.4.tar.gz
+cd nkf-2.1.4
+make -j4
 
 date

44a33721a8c37050437a310106479cc2da776fb4
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-03-31 20:38:59 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -4,9 +4,6 @@ set -x
 
 date
 
-php -l loggly.php
-php -l loggly_error.php
-
 tar xf phpPgAdmin-5.1.tar.bz2
 
 mv phpPgAdmin-5.1 www/phppgadmin
@@ -16,5 +13,7 @@ rm -f phpPgAdmin-5.1.tar.bz2
 cp config.inc.php www/phppgadmin/conf/config.inc.php
 
 chmod 755 ./start_web.sh
+
+
 
 date

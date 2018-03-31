85bbca1d4b06a05e532f42ba70fbbbaa0027c96c
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-03-31 20:43:28 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -9,11 +9,6 @@ httpd -M
 php --version
 whereis php
 
-if [ ! -v LOGGLY_TOKEN ]; then
-  echo "Error : LOGGLY_TOKEN not defined."
-  exit
-fi
-
 if [ ! -v LOG_LEVEL ]; then
   export LOG_LEVEL="warn"
 fi
@@ -29,26 +24,5 @@ if [ ! -v BASIC_PASSWORD ]; then
 fi
 
 htpasswd -c -b .htpasswd ${BASIC_USER} ${BASIC_PASSWORD}
-
-postgres_user=$(echo ${DATABASE_URL} | awk -F':' '{print $2}' | sed -e 's/\///g')
-postgres_password=$(echo ${DATABASE_URL} | grep -o '/.\+@' | grep -o ':.\+' | sed -e 's/://' | sed -e 's/@//')
-postgres_server=$(echo ${DATABASE_URL} | awk -F'@' '{print $2}' | awk -F':' '{print $1}')
-postgres_dbname=$(echo ${DATABASE_URL} | awk -F'/' '{print $NF}')
-
-export PGPASSWORD=${postgres_password}
-
-psql -U ${postgres_user} -d ${postgres_dbname} -h ${postgres_server} > /tmp/sql_result.txt << __HEREDOC__
-SELECT 't_file_hash', COUNT('X')
-  FROM t_file_hash
-__HEREDOC__
-cat /tmp/sql_result.txt
-
-psql -U ${postgres_user} -d ${postgres_dbname} -h ${postgres_server} > /tmp/sql_result.txt << __HEREDOC__
-DELETE
-  FROM t_file_hash
- WHERE create_time < localtimestamp - interval '3 days'
-__HEREDOC__
-
-unset PGPASSWORD
 
 vendor/bin/heroku-php-apache2 -C apache.conf www

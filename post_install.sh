5c186467c956a1d761815545be9a801c58c31e7e
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-07-09 01:17:02 +0900

    Update and rename postinstall.sh to post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -16,6 +16,15 @@ chmod 755 ./start_web.sh
 
 cd /tmp
 
+wget http://download.pureftpd.org/pub/pure-ftpd/misc/libpuzzle/releases/libpuzzle-0.11.tar.bz2
 
+tar xf libpuzzle-0.11.tar.bz2
+
+cd libpuzzle-0.11
+
+./configure --help
+./configure
+
+make -j4
 
 date

23d74930bae20682aed3c6aeffb2e12f27e546f2
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-07-09 01:15:49 +0900

    Create postinstall.sh

---
+++ postinstall.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -x
+
+date
+
+tar xf phpPgAdmin-5.1.tar.bz2
+
+mv phpPgAdmin-5.1 www/phppgadmin
+
+rm -f phpPgAdmin-5.1.tar.bz2
+
+cp config.inc.php www/phppgadmin/conf/config.inc.php
+
+chmod 755 ./start_web.sh
+
+cd /tmp
+
+
+
+date

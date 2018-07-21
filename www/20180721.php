9bfe0d25225d1228657e9d681af08de06d196af5
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-07-22 00:15:16 +0900

    Update 20180721.php

--- www/20180721.php
+++ www/20180721.php
@@ -6,11 +6,16 @@ error_log($url);
 
 $res = file_get_contents($url);
 
+$rc = preg_match('/u=(.+?)</', $res, $matches);
+
+error_log(var_export($matches, true));
+
+/*
 $rc = preg_match('/<div class="gotoBlog"><a href="(.+?)"/', $res, $matches);
 $url = $matches[1];
 
 error_log($url);
-
+*/
 
 
 

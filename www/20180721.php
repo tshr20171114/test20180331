00219be7298e3276799d33e3d3eebe827c2e8d4e
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-07-22 00:18:30 +0900

    Update 20180721.php

--- www/20180721.php
+++ www/20180721.php
@@ -6,7 +6,7 @@ error_log($url);
 
 $res = file_get_contents($url);
 
-$rc = preg_match('/u=(.+?)</', $res, $matches);
+$rc = preg_match_all('/u=(.+?)</', $res, $matches);
 
 error_log(var_export($matches, true));
 

68f44ef5b4b924e609bb0e79738f90bd670ca3cf
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2019-01-05 01:54:33 +0900

    Update 20190105.php

--- www/20190105.php
+++ www/20190105.php
@@ -1,8 +1,19 @@
 <?php
 
-$url = getenv('URL_010');
+$options = [
+        CURLOPT_URL => $url,
+        CURLOPT_USERAGENT => getenv('USER_AGENT'),
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_FOLLOWLOCATION => 1,
+        CURLOPT_MAXREDIRS => 3,
+        CURLOPT_SSL_FALSESTART => true,
+        ];
 
-$res = file_get_contents($url);
-
-error_log($res);
+$ch = curl_init();
+curl_setopt_array($ch, $options);
+$res = curl_exec($ch);
+$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+error_log('HTTP STATUS CODE : ' . $http_code);
+curl_close($ch);
 

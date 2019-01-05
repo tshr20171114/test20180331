948169feb7a4b9c198f5425f15c5b468a4471e41
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2019-01-05 10:13:40 +0900

    Update 20190101.php

--- www/20190101.php
+++ www/20190101.php
@@ -5,7 +5,11 @@ $rc = ftp_login($ftp_link_id, getenv('Test02'), getenv('Test03'));
 
 error_log($rc);
 
-$rc = ftp_put($ftp_link_id, '20190102.php', '20190101.php', FTP_ASCII);
+$rc = ftp_rawlist($ftp_link_id, './');
+
+error_log(print_r($rc, true));
+
+//$rc = ftp_put($ftp_link_id, '20190102.php', '20190101.php', FTP_ASCII);
 
 error_log($rc);
 
